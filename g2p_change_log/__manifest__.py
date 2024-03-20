c45717f307aab64e3b440fffd9584943201f7788
Dibik <dibik@openg2p.org>
2024-03-20 15:55:24 +0530

    Removed changelog module

--- g2p_change_log/__manifest__.py
+++ g2p_change_log/__manifest__.py
@@ -8,14 +8,10 @@
     "license": "Other OSI approved licence",
     "development_status": "Alpha",
     # any module necessary for this one to work correctly
-    "depends": ["base", "auditlog"],
+    "depends": ["base"],
     # always loaded
     "data": [
         # 'security/ir.model.access.csv',
-        "data/auditlog_rule_data.xml",
-        "views/rule_view.xml",
-        "views/menu_view.xml",
-        "views/audit_log_view.xml",
     ],
     "assets": {},
     "external_dependencies": {},
